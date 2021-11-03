b64a4918a7570953fc88d560f27b80689829b607
ArgenX1 <djc.arizona@gmail.com>
2021-11-03 11:02:08 -0700

    working routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const frontEndRoutes = require("./frontEndRoutes.js");
+const frontEndRoutes = require("./frontEndRoutes");
 router.use(frontEndRoutes);
-const apiRoutes = require("./api");
+const apiRoutes = require("./apiRoutes/index");
 router.use("/api",apiRoutes);
-const sessionRoutes = require("./sessionsRoutes")
+const sessionRoutes = require("./sessionsRouter")
 router.use("/sessions",sessionRoutes)
 
 module.exports = router;
